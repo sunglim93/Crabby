a687e07443a4b7d0802d567f144f75fe832c6a53
Cassidy <94720127+cfemling@users.noreply.github.com>
2023-11-21 21:51:59 -0800

    Added Data CSVs and Skeleton File
    Included the csv's from the kaggle challenge and created a python file for us to work on with just a main function and some imports

---
+++ crabby.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.compose import TransformedTargetRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LinearRegression
+
+def main():
+    print("hello world")
+
+if __name__ == "__main__":
+    main()
